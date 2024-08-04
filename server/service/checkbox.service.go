142fc42d233c64667d6453141894245994be1191
veronb <veron.b@hsenidoutsourcing.com>
2024-08-04 16:46:52 +0530

    Add service method to return RLE encoded value

--- server/service/checkbox.service.go
+++ server/service/checkbox.service.go
@@ -8,12 +8,21 @@ type CheckBoxService struct {
 	bitSetMap lib.BitSetMap
 }
 
+const (
+	maxCheckboxCount = 1000000
+)
+
 func NewCheckBoxService() *CheckBoxService {
 	return &CheckBoxService{
-		bitSetMap: *lib.NewBitSetMap(),
+		bitSetMap: *lib.NewBitSetMap(maxCheckboxCount),
 	}
 }
 
 func (cbs *CheckBoxService) SetValue(position uint32, isChecked bool) {
 	cbs.bitSetMap.Set(position, isChecked)
+}
+
+func (cbs *CheckBoxService) GetCheckboxesEncodedValue() string {
+	rle := lib.NewRunLengthEncoder(cbs.bitSetMap.GetMap())
+	return rle.GetEncodedValue()
 }
