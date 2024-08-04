8cd7b9e54bc1b4c00b93b7b4cb45cf8336d5f20b
veronb <veron.b@hsenidoutsourcing.com>
2024-08-04 14:49:47 +0530

    Implement concurrent safe bitset map

--- server/lib/bitsetmap.go
+++ server/lib/bitsetmap.go
@@ -1,20 +1,45 @@
 package lib
 
-type BitSetMap map[uint32]bool
+import (
+	"log"
+	"sync"
+)
 
-func NewBitSetMap() *BitSetMap {
-	bitSetMap := make(BitSetMap)
-	return &bitSetMap
+type BitSetMap struct {
+	data map[uint32]bool
+	rwMu sync.RWMutex
 }
 
-func (b *BitSetMap) Set(position uint32, value bool) {
-	if value {
-		(*b)[position] = value
-	} else {
-		delete((*b), position)
+func NewBitSetMap(size int) *BitSetMap {
+	bitSetMap := make(map[uint32]bool)
+	for i := 1; i <= size; i++ {
+		bitSetMap[uint32(i)] = false
+	}
+	return &BitSetMap{
+		data: bitSetMap,
 	}
 }
 
-func (b BitSetMap) Get(position uint32) bool {
-	return b[position]
+func (b *BitSetMap) Set(position uint32, value bool) {
+	b.rwMu.Lock()
+	log.Printf("Setting position: %d to isChecked: %t", position, value)
+	(*b).data[uint32(position)] = value
+	b.rwMu.Unlock()
+}
+
+func (b *BitSetMap) Get(position uint32) bool {
+	b.rwMu.RLock()
+	defer b.rwMu.RUnlock()
+	return b.data[position]
+}
+
+func (b *BitSetMap) GetMap() map[uint32]bool {
+	b.rwMu.RLock()
+	defer b.rwMu.RUnlock()
+
+	copyMap := make(map[uint32]bool, len(b.data))
+	for k, v := range b.data {
+		copyMap[k] = v
+	}
+	return copyMap
 }
--- server/service/checkbox.service.go
+++ server/service/checkbox.service.go
@@ -1,1 +1,19 @@
 package service
+
+import (
+	"github.com/veron-baranige/one-million-checkboxes/lib"
+)
+
+type CheckBoxService struct {
+	bitSetMap lib.BitSetMap
+}
+
+func NewCheckBoxService() *CheckBoxService {
+	return &CheckBoxService{
+		bitSetMap: *lib.NewBitSetMap(),
+	}
+}
+
+func (cbs *CheckBoxService) SetValue(position uint32, isChecked bool) {
+	cbs.bitSetMap.Set(position, isChecked)
+}
